541fc65aee054d457ff6c047ab935c938977c953
ali <starntopdevwork@gmail.com>
2024-07-30 13:50:59 +0500

    Delete src folder, remove footer import

--- my-portfolio/pages/index.js
+++ my-portfolio/pages/index.js
@@ -1,14 +1,12 @@
 import Layout from '../components/Layout';
 import Header from '../components/Header';
 import Main from '../components/Main';
-import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <Layout>
       <Header />
       <Main />
-      <Footer />
     </Layout>
   );
 };
